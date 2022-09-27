cc1ff2f62beb297e68838e3087594c691466db5c
2211115 <2211115@cognizant.com>
2022-09-27 16:05:00 -0400

    adding spring boot build

---
+++ turtlemart/src/main/java/com/example/turtlemart/TurtlemartApplication.java
@@ -0,0 +1,13 @@
+package com.example.turtlemart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TurtlemartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TurtlemartApplication.class, args);
+	}
+
+}
