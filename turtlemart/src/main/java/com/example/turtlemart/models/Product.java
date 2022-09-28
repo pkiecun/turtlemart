9828b90ca730ec1d6449467793b59fe6cda74837
pkiecun <103776764+pkiecun@users.noreply.github.com>
2022-09-28 22:12:32 +0530

    Update Product.java

--- turtlemart/src/main/java/com/example/turtlemart/models/Product.java
+++ turtlemart/src/main/java/com/example/turtlemart/models/Product.java
@@ -2,15 +2,25 @@ package com.example.turtlemart.models;
 
 import lombok.*;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+@Entity
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
 @Setter
 public class Product {
-    private int product_Id;
 
-    private String product_name;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int item;
 
-    private int department_Id;
+    private String product;
+
+    private int identity;
+
+    private String department;
 
 }
