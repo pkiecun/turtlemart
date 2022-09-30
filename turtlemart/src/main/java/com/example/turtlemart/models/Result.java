0febde2e2b7ec0b5d903b25123cd246404f5ef3c
Tron <81943236+tron-stack@users.noreply.github.com>
2022-09-30 10:44:15 -0500

    Update Result.java

--- turtlemart/src/main/java/com/example/turtlemart/models/Result.java
+++ turtlemart/src/main/java/com/example/turtlemart/models/Result.java
@@ -2,6 +2,7 @@ package com.example.turtlemart.models;
 
 import lombok.*;
 
+import java.util.List;
 import java.util.Map;
 
 @AllArgsConstructor
@@ -11,5 +12,5 @@ import java.util.Map;
 public class Result {
     private String location_Name;
     private int zip_Code;
-    private Map<String, Double> inventory;
+    private List<Inventory> inventory;
 }
