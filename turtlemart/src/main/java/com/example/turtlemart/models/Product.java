55ae6bd00e01eb14f937996c45ad401f3cc29b22
Tron <81943236+tron-stack@users.noreply.github.com>
2022-09-30 10:43:54 -0500

    Update Product.java

--- turtlemart/src/main/java/com/example/turtlemart/models/Product.java
+++ turtlemart/src/main/java/com/example/turtlemart/models/Product.java
@@ -2,15 +2,25 @@ package com.example.turtlemart.models;
 
 import lombok.*;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+@Entity
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
 @Setter
 public class Product {
-    private int product_Id;
 
-    private String product_name;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int item;
 
-    private int department_Id;
+    private String product;
+
+    private int deptid;
+
+    private String department;
 
 }
