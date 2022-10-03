c09c13d5e34c8067b053e05b5abca96632a08794
Tron <81943236+tron-stack@users.noreply.github.com>
2022-10-03 10:55:35 -0500

    Create PostCodeArray.java

---
+++ turtlemart/src/main/java/com/example/turtlemart/models/PostCodeArray.java
@@ -0,0 +1,15 @@
+package com.example.turtlemart.models;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class PostCodeArray {
+    private String[] postalCodes;
+}
